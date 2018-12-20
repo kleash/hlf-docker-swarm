74b6d01fc607edf7edc6ad7037f2d00545e7a279
shubham <shubham6300@gmail.com>
2018-12-20 18:58:11 +0530

    changed to 2 org

--- network/populate_hostname.sh
+++ network/populate_hostname.sh
@@ -15,8 +15,8 @@ elif [ "$ARCH" == "Darwin" ]; then
 fi
 
 # Zookeeper + Kafka
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $ZK_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $KAFKA_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $ZK_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $KAFKA_COMPOSE_PATH
 
 # ORG1
 ORG1_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org1.example.com/ca/ | grep "_sk")
@@ -32,41 +32,6 @@ sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $ORDER
 sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $PEER_ORG2_COMPOSE_PATH
 sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $SERVICE_ORG2_COMPOSE_PATH
 sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG2_CA_PATH#g" $SERVICE_ORG2_COMPOSE_PATH
-
-# ORG3
-ORG3_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org3.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $ORDERER2_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $PEER_ORG3_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG3_HOSTNAME/g" $SERVICE_ORG3_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG3_CA_PATH#g" $SERVICE_ORG3_COMPOSE_PATH
-
-
-# ORG4
-ORG4_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org4.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG4_HOSTNAME/g" $PEER_ORG4_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG4_HOSTNAME/g" $SERVICE_ORG4_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG4_CA_PATH#g" $SERVICE_ORG4_COMPOSE_PATH
-
-
-# ORG5
-ORG5_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org5.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG5_HOSTNAME/g" $PEER_ORG5_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG5_HOSTNAME/g" $SERVICE_ORG5_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG5_CA_PATH#g" $SERVICE_ORG5_COMPOSE_PATH
-
-
-# ORG6
-ORG6_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org6.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG6_HOSTNAME/g" $PEER_ORG6_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG6_HOSTNAME/g" $SERVICE_ORG6_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG6_CA_PATH#g" $SERVICE_ORG6_COMPOSE_PATH
-
-# ORG7
-ORG7_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org7.example.com/ca/ | grep "_sk")
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG7_HOSTNAME/g" $PEER_ORG7_COMPOSE_PATH
-sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG7_HOSTNAME/g" $SERVICE_ORG7_COMPOSE_PATH
-sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG7_CA_PATH#g" $SERVICE_ORG7_COMPOSE_PATH
-
 
 if [ "$ARCH" == "Darwin" ]; then
   rm */**.ymlt
--- network/start_all.sh
+++ network/start_all.sh
@@ -1,11 +1,6 @@
 ./scripts/network/deploy_services_kafka.sh
 ./scripts/network/deploy_services_org1.sh
 ./scripts/network/deploy_services_org2.sh
-./scripts/network/deploy_services_org3.sh
-./scripts/network/deploy_services_org4.sh
-./scripts/network/deploy_services_org5.sh
-./scripts/network/deploy_services_org6.sh
-./scripts/network/deploy_services_org7.sh
 
 
 
