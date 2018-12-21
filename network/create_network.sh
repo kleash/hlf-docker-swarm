2a15be2457ad756d34e9fc8d08c1338285f14b50
shubham <shubham6300@gmail.com>
2018-12-21 16:48:21 +0530

    changed default dir from var to home efs mount

--- network/create_network.sh
+++ network/create_network.sh
@@ -4,4 +4,4 @@ ENV_LOCATION=$PWD/.env
 echo $ENV_LOCATION
 source $ENV_LOCATION
 
-docker network create --driver overlay --subnet=10.200.1.0/24 --attachable "$NETWORK_NAME"
+docker network create --driver overlay --attachable "$NETWORK_NAME"
--- network/move_crypto.sh
+++ network/move_crypto.sh
@@ -1,18 +1,17 @@
 # sudo mkdir -p /var/mynetwork
 # sudo chown -R $(whoami) /var/mynetwork
 # sudo chown -R $USER:$USER /var/mynetwork
-rm -rf mkdir /var/mynetwork/*
+rm -rf mkdir /home/ubuntu/efsmount/mynetwork/*
 
-mkdir -p /var/mynetwork/chaincode
-mkdir -p /var/mynetwork/certs
-mkdir -p /var/mynetwork/bin
-mkdir -p /var/mynetwork/fabric-src
+mkdir -p /home/ubuntu/efsmount/mynetwork/chaincode
+mkdir -p /home/ubuntu/efsmount/mynetwork/certs
+mkdir -p /home/ubuntu/efsmount/mynetwork/bin
 
-git clone https://github.com/hyperledger/fabric /var/mynetwork/fabric-src/hyperledger/fabric
-cd /var/mynetwork/fabric-src/hyperledger/fabric
-git checkout release-1.1
+# git clone https://github.com/hyperledger/fabric /var/mynetwork/fabric-src/hyperledger/fabric
+#cd /var/mynetwork/fabric-src/hyperledger/fabric
+#git checkout release-1.1
 cd -
-cp -R crypto-config /var/mynetwork/certs/
-cp -R config /var/mynetwork/certs/
-cp -R ../chaincodes/* /var/mynetwork/chaincode/
-cp -R bin/* /var/mynetwork/bin/
+cp -R crypto-config /home/ubuntu/efsmount/mynetwork/certs/
+cp -R config /home/ubuntu/efsmount/mynetwork/certs/
+cp -R ../chaincodes/* /home/ubuntu/efsmount/mynetwork/chaincode/
+cp -R bin/* /home/ubuntu/efsmount/mynetwork/bin/
