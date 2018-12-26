26a96d1ef4460ba48f0e8e9bf975be39aee2d3ec
Manindra Singh <manindra23@yahoo.co.in>
2018-12-26 16:25:27 +0530

    exporting .env

--- network/start_all.sh
+++ network/start_all.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+set -ev
+
+ENV_LOCATION=$PWD/.env
+echo $ENV_LOCATION
+source $ENV_LOCATION
+
 ./scripts/network/deploy_services_kafka.sh
 ./scripts/network/deploy_services_org1.sh
 ./scripts/network/deploy_services_org2.sh
